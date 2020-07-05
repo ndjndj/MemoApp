e47209efcd07c8a21a4d3fa64f9f0bdb1989dd8b
ndjndj <teq.ndj@gmail.com>
2020-07-06 01:21:00 +0900

    bodytext

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import BodyText from './src/elements/BodyText';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello!</Text>
       <StatusBar style="auto" />
+      <BodyText>HI!</BodyText>
     </View>
   );
 }
