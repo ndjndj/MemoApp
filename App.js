6255006afc8d54024eee250a2438d29483bc2a54
ndjndj <teq.ndj@gmail.com>
2020-07-08 00:09:46 +0900

    memodetailscreen

--- App.js
+++ App.js
@@ -2,13 +2,14 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Appbar from './src/components/Appbar';
-import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Appbar />
-      <MemoListScreen />
+      <MemoDetailScreen />
     </View>
   );
 }
@@ -18,6 +19,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fffDf6',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 78,
   },
 });
