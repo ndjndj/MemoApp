522df8e95e4bcc53b6fadc55887905169d66d317
ndjndj <teq.ndj@gmail.com>
2020-07-12 20:51:04 +0900

    ~chapter70

--- App.js
+++ App.js
@@ -1,28 +1,13 @@
-// import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Appbar from './src/components/Appbar';
-// import MemoListScreen from './src/screens/MemoListScreen';
-// import MemoDetailScreen from './src/screens/MemoDetailScreen';
-// import MemoEditScreen from './src/screens/MemoEditScreen';
-// import LoginScreen from './src/screens/LoginScreen';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
+import LoginScreen from './src/screens/LoginScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Appbar />
-      <SignUpScreen />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fffDf6',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    paddingTop: 78,
-  },
+const App = createStackNavigator({
+  Home: { screen: MemoListScreen },
 });
+
+export default createAppContainer(App);
